cf47f2bf03c281323aa95e30be4404bc69ad75aa
LeRoy Dahl <ldahl2018@gmail.com>
2020-10-08 09:13:47 -0500

    updating notes

--- server/server.js
+++ server/server.js
@@ -1,21 +1,32 @@
 // Require express - gives us a function
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // Create an instance of express by calling the function 
 // returned above - gives us an object
 const app = express();
 const port = 5000;
 
-const quotesData = require ('./modules/quotes.js');
+const quotesData = require('./modules/quotes.js');
 
 // express static file serving - public is the folder name
 app.use(express.static('server/public'));
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 
 let index = 0;
 
+app.post('/quotes', (req, res) => {
+    console.log('hi from post request', req.body);
+
+    quotesData.push(req.body)
+    res.send(quotesData);
+})
+// GET  retrieves data from 
 app.get('/quotes', (req, res) => {
-    console.log('hi from get request');
+    console.log('hi from get request', req.body);
     res.send(quotesData);
 })
 
