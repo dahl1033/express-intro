6e515eadd8c9227ab982fd0872ef81c98ca0dbca
LeRoy Dahl <ldahl2018@gmail.com>
2020-10-08 10:31:21 -0500

    updated code

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,26 +2,55 @@ console.log("hello from JS");
 
 $(document).ready(onReady);
 
-function onReady(){
-    console.log('hello from jquery');
-    // GET request
-    getRandomQuote();
+function onReady() {
+    // perform GET request
+    getQuotes();
+
+    $('#submit').on('click', submitQuote);
 }
 
-function getRandomQuote(){
-    console.log('getting random quote');
+function submitQuote() {
+    // grabbing value from the dom
+    let quote = $('#quote').val();
+    let author = $('#author').val();
+    console.log('clicked', quote, author);
+    // send data to server via post request
+    $.ajax({
+        method: 'POST',
+        url: '/submitQuotes',
+        data: {
+            quote: quote,
+            author: author
+        }
+    }).then(function (response) {
+        console.log('response: ', response);
+        getQuotes();
+    }).catch(function (error) {
+        //notifying the user of an error in post request
+        alert(error);
+    });
+}
+
+
+function getQuotes() {
+    console.log("get the quote");
     $.ajax({
         method: 'GET',
-        url: '/randomQuote'
-        // response is the res.send in server.js
-    }).then(function(response){
-        //response from server
-        console.log('response =', response);
+        url: '/quotes'
+    }).then(function (response) {
+        console.log('response', response);
         appendToDom(response);
     });
 }
 
-function appendToDom(dataToAppend){
+function appendToDom(dataToAppend) {
     $('#output').empty();
-    $('#output').append(dataToAppend.quote,'<br/>~ ', dataToAppend.author);
+    // take the response from the server
+    // append it to #output so it shows up on the DOM
+    for (let i = 0; i < dataToAppend.length; i++) {
+        $('#output').append(`
+            <p>${dataToAppend[i].quote}</p>
+            <i>from: ${dataToAppend[i].author}</i>
+        `)
+    }
 }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 // Require express - gives us a function
-
 const express = require('express');
+// require body-parser to parse through data
 const bodyParser = require('body-parser');
-
 // Create an instance of express by calling the function 
 // returned above - gives us an object
 const app = express();
@@ -13,31 +12,32 @@ const quotesData = require('./modules/quotes.js');
 // express static file serving - public is the folder name
 app.use(express.static('server/public'));
 
-app.use(bodyParser.urlencoded({extended: true}))
-
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 let index = 0;
 
-app.post('/quotes', (req, res) => {
-    console.log('hi from post request', req.body);
-
-    quotesData.push(req.body)
-    res.send(quotesData);
-})
-// GET  retrieves data from 
 app.get('/quotes', (req, res) => {
-    console.log('hi from get request', req.body);
-    res.send(quotesData);
+    console.log('hi from get request');
+    res.send(quotesData.list);
 })
 
 app.get('/randomQuote', (req, res) => {
-    let randomNumber = getRandomInt(quotesData.length);
-    res.send(quotesData[randomNumber]);
+    let randomNumber = getRandomInt(quotesData.list.length);
+    res.send(quotesData.list[randomNumber]);
 });
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
-  }
+}
+
+app.post('/submitQuotes', (req, res) => {
+    console.log('hello from post', req.body);
+    //push req.body to our quotesData array
+    quotesData.list.push(req.body);
+    res.sendStatus(200);
+});
 
 app.listen(port, () => {
     console.log("Up and running on port: ", port);
