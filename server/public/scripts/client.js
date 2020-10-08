39ccd113b3d073d0c6c3b9ae3e2bc65f5d65fa6b
LeRoy Dahl <ldahl2018@gmail.com>
2020-10-08 09:13:56 -0500

    updating git

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,27 @@
 console.log("hello from JS");
+
+$(document).ready(onReady);
+
+function onReady(){
+    console.log('hello from jquery');
+    // GET request
+    getRandomQuote();
+}
+
+function getRandomQuote(){
+    console.log('getting random quote');
+    $.ajax({
+        method: 'GET',
+        url: '/randomQuote'
+        // response is the res.send in server.js
+    }).then(function(response){
+        //response from server
+        console.log('response =', response);
+        appendToDom(response);
+    });
+}
+
+function appendToDom(dataToAppend){
+    $('#output').empty();
+    $('#output').append(dataToAppend.quote,'<br/>~ ', dataToAppend.author);
+}
